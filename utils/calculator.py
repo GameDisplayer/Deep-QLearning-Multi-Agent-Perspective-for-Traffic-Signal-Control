d4bfc6e7a557fe1bee0c218e664a509b7426e1b3
Romain <romainmichelucci@protonmail.com>
2021-08-23 23:36:19 +0200

    Mini python function
    for calculating percentage differences #23

---
+++ utils/calculator.py
@@ -0,0 +1,71 @@
+def percentage_difference(base, new):
+    return round((new - base) / base * 100)
+
+
+##print("WITHOUT ART QUEUE")
+##print("Low")
+##print(percentage_difference(-5985.4, ))
+##print(percentage_difference(15709.2, ))
+##print(percentage_difference(37.3, ))
+##
+##print("High")
+##print(percentage_difference(-129757.8, ))
+##print(percentage_difference(606655.6, ))
+##print(percentage_difference(111.6, ))
+##
+##print("NS")
+##print(percentage_difference(-81487.6, ))
+##print(percentage_difference(146007.0, ))
+##print(percentage_difference(61.6, ))
+##
+##print("EW")
+##print(percentage_difference(-79461.6, ))
+##print(percentage_difference(143470.2, ))
+##print(percentage_difference(60.3, ))
+##
+
+
+##print("WITH ART QUEUE")
+##print("Low")
+##print(percentage_difference(5985.8, 3804.0))
+##print(percentage_difference(15703.6, 8162.8))
+##print(percentage_difference(35.9, 19.3))
+##
+##print("High")
+##print(percentage_difference(144427.4, 125444.6))
+##print(percentage_difference(650104.6, 574584.4))
+##print(percentage_difference(112.7, 115.7))
+##
+##print("NS")
+##print(percentage_difference(82265.2, 10432.8))
+##print(percentage_difference(148309.2, 39071.0))
+##print(percentage_difference(56.5, 18.6))
+##
+##print("EW")
+##print(percentage_difference(80653.8, 13145.8))
+##print(percentage_difference(146202.4, 43675.4))
+##print(percentage_difference(53.1, 22.1))
+
+#print(percentage_difference(,))
+
+##print("TWO-INTERSECTIONS")
+##print("Low")
+##print(percentage_difference(-10678.8, -35202.2))
+##print(percentage_difference(22603.4, 159644.2))
+##print(percentage_difference(43.3, 264.6))
+##
+##print("High")
+##print(percentage_difference(-1459829.8, -1233550.9))
+##print(percentage_difference(2430910.4, 6795419.2))
+##print(percentage_difference(594.1, 1981.3))
+##
+##print("NS")
+##print(percentage_difference(-28892.6, -300984.1))
+##print(percentage_difference(73343.8, 1502698.4))
+##print(percentage_difference(37.5, 806.6))
+##
+##print("EW")
+##print(percentage_difference(-438308.0, -35314.4))
+##print(percentage_difference(676792.8, 239661.0))
+##print(percentage_difference(303.1, 121.1))
+
